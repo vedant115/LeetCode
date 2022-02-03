a63191d9911a9c729334f924cf1239bd8e2e2813
Vedant Rahane <71424483+vedant115@users.noreply.github.com>
2022-02-03 08:30:54 +0530

    Update 4Sum_II.py

--- Daily LeetCode Challenge/February/4Sum_II.py
+++ Daily LeetCode Challenge/February/4Sum_II.py
@@ -32,3 +32,11 @@ class Solution:
                 ans+=count1.get(-(c+d), 0)
                 
         return ans
+
+# Same approach short 
+
+class Solution:
+    def fourSumCount(self, nums1: List[int], nums2: List[int], nums3: List[int], nums4: List[int]) -> int:
+        count = Counter(a + b for a in nums1 for b in nums2)
+        return sum(count[-c-d] for c in nums3 for d in nums4)
+    
