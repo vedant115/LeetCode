4b7071a750624f0a618861058323db4431ebf95b
Vedant Rahane <71424483+vedant115@users.noreply.github.com>
2022-02-19 22:48:10 +0530

    Update Minimize_Deviation_in_Array.py

--- Daily LeetCode Challenge/February/Minimize_Deviation_in_Array.py
+++ Daily LeetCode Challenge/February/Minimize_Deviation_in_Array.py
@@ -21,32 +21,27 @@ Explanation: You can transform the array to [1,2,3,2], then to [2,2,3,2], then t
 '''
 
 def minimumDeviation(self, nums: List[int]) -> int:
-
-		from sortedcontainers import SortedList
-
-		for i in range(len(nums)):
-
-			if nums[i]%2!=0:
-				nums[i]=nums[i]*2
-
-		nums = SortedList(nums)
-
-		result = 100000000000
-
-		while True:
-			min_value = nums[0]
+	from sortedcontainers import SortedList
+	
+	for i in range(len(nums)):
+		if nums[i]%2!=0:
+			nums[i]=nums[i]*2
+	
+	nums = SortedList(nums)
+	
+	result = 100000000000
+	
+	while True:
+		min_value = nums[0]
+		max_value = nums[-1]
+		if max_value % 2 == 0:
+			nums.pop()
+			nums.add(max_value // 2)
 			max_value = nums[-1]
-
-			if max_value % 2 == 0:
-				nums.pop()
-				nums.add(max_value // 2)
-				max_value = nums[-1]
-				min_value = nums[0]
-
-				result = min(result , max_value - min_value)
-			else:
-				result = min(result , max_value - min_value)
-				break
-
-		return result
+			min_value = nums[0]
+			result = min(result , max_value - min_value)
+		else:
+			result = min(result , max_value - min_value)
+			break
+	return result
     
