e2349710ee6e6050abb290c86d5aa148655f5b39
Vedant Rahane <71424483+vedant115@users.noreply.github.com>
2022-02-22 22:34:35 +0530

    Create Excel_Sheet_Column_Number.py

---
+++ Daily LeetCode Challenge/February/Excel_Sheet_Column_Number.py
@@ -0,0 +1,43 @@
+# LeetCode 171
+
+'''
+Given a string columnTitle that represents the column title as appear in an Excel sheet, return its corresponding column number.
+
+For example:
+
+A -> 1
+B -> 2
+C -> 3
+...
+Z -> 26
+AA -> 27
+AB -> 28 
+...
+
+Input: columnTitle = "AB"
+Output: 28
+Example 3:
+
+Input: columnTitle = "ZY"
+Output: 701
+'''
+
+def titleToNumber(self, columnTitle: str) -> int:
+    d = {'A': 1, 'B': 2, 'C': 3, 'D': 4, 'E': 5, 'F': 6, 'G': 7, 'H': 8, 'I': 9, 'J': 10, 'K': 11, 'L': 12, 'M': 13, 'N': 14, 'O': 15, 'P': 16, 'Q': 17, 'R': 18, 'S': 19, 'T': 20, 'U': 21, 'V': 22, 'W': 23, 'X': 24, 'Y': 25, 'Z': 26}
+    ans = 0
+		
+    for i in columnTitle:
+        ans = ans * 26 + d[i]
+    return ans
+	
+def titleToNumber(self, columnTitle: str) -> int:
+    d = {'A': 1, 'B': 2, 'C': 3, 'D': 4, 'E': 5, 'F': 6, 'G': 7, 'H': 8, 'I': 9, 'J': 10, 'K': 11, 'L': 12, 'M': 13, 'N': 14, 'O': 15, 'P': 16, 'Q': 17, 'R': 18, 'S': 19, 'T': 20, 'U': 21, 'V': 22, 'W': 23, 'X': 24, 'Y': 25, 'Z': 26}
+    ans, pos = 0, 0
+		
+    for letter in reversed(columnTitle):
+        digit = d[letter]
+        ans += digit * 26**pos
+        pos += 1
+        
+    return ans
+	
