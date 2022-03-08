b24b03ad615627c0397a2109adc4b064bf7f3afb
Vedant Rahane <71424483+vedant115@users.noreply.github.com>
2022-03-08 23:50:44 +0530

    Create Linked_List_Cycle.py

---
+++ Daily LeetCode Challenge/March/Linked_List_Cycle.py
@@ -0,0 +1,40 @@
+# LeetCode 141. 
+
+"""
+Given head, the head of a linked list, determine if the linked list has a cycle in it.
+
+There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer. Internally, pos is used to denote the index of the node that tail's next pointer is connected to. Note that pos is not passed as a parameter.
+
+Return true if there is a cycle in the linked list. Otherwise, return false.
+
+
+Example 1:
+Input: head = [3,2,0,-4], pos = 1
+Output: true
+
+Explanation: There is a cycle in the linked list, where the tail connects to the 1st node (0-indexed).
+"""
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        
+        if head is None:
+            return False
+        
+        slow = head 
+        fast = head.next
+        
+        while fast != slow:
+            if fast is None or fast.next is None:
+                return False
+            slow = slow.next 
+            fast = fast.next.next
+                
+        return True
+			
