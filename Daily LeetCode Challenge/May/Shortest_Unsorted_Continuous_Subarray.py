adb05a2e37f566a2d5e12e8a2e7b897a02e7206f
Vedant Rahane <71424483+vedant115@users.noreply.github.com>
2022-05-03 22:42:48 +0530

    Create Shortest_Unsorted_Continuous_Subarray.py

---
+++ Daily LeetCode Challenge/May/Shortest_Unsorted_Continuous_Subarray.py
@@ -0,0 +1,40 @@
+# LeetCode 581. 
+
+"""
+Given an integer array nums, you need to find one continuous subarray that if you only sort this subarray in ascending order, then the whole array will be sorted in ascending order.
+
+Return the shortest such subarray and output its length.
+
+Example 1:
+
+Input: nums = [2,6,4,8,10,9,15]
+Output: 5
+Explanation: You need to sort [6, 4, 8, 10, 9] in ascending order to make the whole array sorted in ascending order.
+"""
+
+class Solution(object):
+    def findUnsortedSubarray(self, nums):
+        if len(nums) <2:
+            return 0
+        
+        prev = nums[0]
+        end = 0
+		# find the largest index not in place
+        for i in range(0, len(nums)):
+            if nums[i] < prev:
+                end = i
+            else:
+                prev = nums[i]
+
+        start = len(nums) - 1
+        prev = nums[start]
+		# find the smallest index not in place
+        for i in range(len(nums)-1, -1, -1):
+            if prev < nums[i]:
+                start = i
+            else:
+                prev = nums[i]
+        if end != 0:
+            return end - start + 1
+        else: 
+            return 0
